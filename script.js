d9c10b903390c56b545fb928003c613a5bcb00c0
ren132710 <ren132710@gmail.com>
2022-06-22 17:55:14 -0500

    comments

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 /*
  * spike
  *  - understand how SearchBox works
- *  - with and without async, defer attributes
- *  - with and without callback param
- *  - test with parcel
+ *  - async: works if present or not
+ *  - defer: works if present or not
+ *  - type="text/javascript": works if present or not
+ *  //TODO: callback: 'initMap is not a function' error
+ *      if I add function initMap () with a console.log, the url calls the function and prints the console.log
+ *      however then receive 'ReferenceError: google is not defined' and SearchBox no longer works
  */
+
+//this below is currently working both via Live Server and Parcel
 const placeResult = document.querySelector('[data-place-result]')
 const placeSearch = document.querySelector('[data-place-search]')
 console.log(placeSearch)
@@ -20,7 +25,3 @@ searchBox.addListener('places_changed', () => {
   const maps_place_id = place.place_id
   console.log('params: ', lat, long, maps_place_id, location)
 })
-
-function initMap() {
-  console.log('hello')
-}
